bbaab691c9ad88317b4b08c97a547828563bd7eb
Marek Sterzik <marek.sterzik@gmail.com>
2023-04-22 19:15:16 +0200

    remove unused class aliases

--- lib/libsso.php
+++ lib/libsso.php
@@ -5,5 +5,3 @@ require_once __DIR__ . "/classes/SSOUser.php";
 require_once __DIR__ . "/classes/SSOUserPrinter.php";
 
 class_alias(SPSOstrov\SSO\SSO::class, "SSO");
-class_alias(SPSOstrov\SSO\SSOUser::class, "SSOUser");
-class_alias(SPSOstrov\SSO\SSOUserPrinter::class, "SSOUserPrinter");
