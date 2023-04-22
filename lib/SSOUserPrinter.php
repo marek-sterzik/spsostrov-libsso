3277cce4b84917e1dce4bb844af147d9484aad90
Marek Sterzik <marek.sterzik@gmail.com>
2023-04-22 13:30:58 +0200

    app design improvement

--- lib/SSOUserPrinter.php
+++ lib/SSOUserPrinter.php
@@ -1,10 +1,22 @@
 <?php
 
+/**
+ * This class is responsible for pretty-printing the content of the SSOUser object.
+ */
 class SSOUserPrinter
 {
-    public function print(SSOUser $user, bool $return): ?string
+    static bool $cssPrinted = false;
+
+    public function print(SSOUser $user, bool $return = true, ?bool $printCss = null): ?string
     {
+        if ($printCss === null) {
+            $printCss = !self::$cssPrinted;
+            self::$cssPrinted = true;
+        }
         $html = $this->getHtml($user);
+        if ($printCss) {
+            $html = sprintf("<style>%s</style>%s",$this->getCss(), $html);
+        }
         if ($return) {
             return $html;
         }
@@ -12,12 +24,22 @@ class SSOUserPrinter
         return null;
     }
 
+    private function getCss(): string
+    {
+        $css = "";
+        $css .= "table.sso_user_table {background-color: #404040; border-radius: .4em; color: white; box-shadow: 3px 3px 5px #a0a0a0;}\n";
+        $css .= "table.sso_user_table th, table.sso_user_table td {padding: .2em .5em;}\n";
+        $css .= "table.sso_user_table th.table_head {color: red;}\n";
+        return $css;
+    }
+
     private function getHtml($user): string
     {
         $groups = $user->getGroups();
         $groups = empty($groups) ? null : implode(", ", $groups);
         $html = "";
-        $html .= "<table border=\"1\">\n";
+        $html .= "<table class=\"sso_user_table\">\n";
+        $html .= "<tr><th colspan=\"2\" class=\"table_head\">SSO user info</th></tr>\n";
         $html .= $this->getRowHtml("Login", $user->getLogin());
         $html .= $this->getRowHtml("Name", $user->getName());
         $html .= $this->getRowHtml("E-mail", $user->getEmail());
--- testapp/index.php
+++ testapp/index.php
@@ -7,15 +7,23 @@ $user = $_SESSION["user"] ?? null;
 
 ?><!doctype html>
 <html>
+<head>
+    <link rel="stylesheet" href="media/style.css">
+</head>
 <body>
     <h1>SPŠ Ostrov SSO library test application</h1>
-    <div>
-        User currently logged in:
-        <?php echo $user ? $user->prettyPrint(true) : "None"; ?>
-    </div>
-    <div>
-        <a href="sso.php">SSO Login</a>,
-        <a href="sso.php?logout=1">Logout</a>,
+    <div class="userinfo">
+        <?php if($user): ?>
+            <div>User currently logged in:</div>
+            <?php $user->prettyPrint(); ?>
+        <?php else: ?>
+            <div>No user currently logged in.</div>
+            <p class="error">No user info available.</p>
+        <?php endif ?>
+        <div class="links">
+            <a href="sso.php">SSO Login</a>
+            <a href="sso.php?logout=1">Logout</a>
+        </div>
     </div>
 </body>
 </html>
